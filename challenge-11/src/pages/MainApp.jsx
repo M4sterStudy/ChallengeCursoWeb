18fb3342f1b4084fb276336a992adfd2e1a56eee
M4sterStudy <dayan.lara@uao.edu.co>
2023-10-10 15:27:00 -0500

    Challenge 11 incompleto

---
+++ challenge-11/src/pages/MainApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { NavComponent } from '../components/NavComponent'
+import "./stylesApp.css";
+
+export const MainApp = () => {
+  return (
+    <div className="App">
+      <NavComponent/>
+    </div>
+  )
+}
