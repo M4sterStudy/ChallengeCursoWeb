c08cb74c2ea6307763f7b2bd44810ad800c7af2b
M4sterStudy <dayan.lara@uao.edu.co>
2023-10-10 22:14:33 -0500

    cambios del challenges 4

--- CuartoChallenge/src/index.js
+++ CuartoChallenge/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { GifGrid } from './GifGrid';
+import GifExpertApp from './GiftExpertApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GifGrid />
+    <GifExpertApp />
   </React.StrictMode>
 );
 
